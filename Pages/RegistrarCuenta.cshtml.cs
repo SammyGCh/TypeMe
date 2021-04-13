f3fd799dcfa2b9184dc1e5634c048e7e18b65231
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2021-04-12 23:39:03 -0500

    Creación de la página para registrar una cuenta.

---
+++ Pages/RegistrarCuenta.cshtml.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace TypeMeWeb.Pages
+{
+    public class RegistrarCuentaModel : PageModel
+    {
+        [BindProperty]
+        public string EmailPrincipal { get; set; }
+        [BindProperty]
+        public string EmailSecundario { get; set; }
+        [BindProperty]
+        public string Usuario { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+        [BindProperty]
+        public string ConfirmacionPassword { get; set; }
+        [ViewData]
+        public string Resultado { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public RegistrarCuentaModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            bool sonPasswordsIguales = VerificarConfirmacionDePassword();
+
+            if(sonPasswordsIguales)
+            {
+                return new RedirectToPageResult("/Index");
+            }
+            else
+            {
+                
+            }
+
+            return Page();
+        }
+
+        private bool VerificarConfirmacionDePassword()
+        {
+            return Password.Equals(ConfirmacionPassword);
+        }
+    }
+}
