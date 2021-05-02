93366242e171045c101124472e013baf42b6f99c
lzeroxl <Angel_IS2020@hotmail.com>
2021-05-02 16:44:37 -0500

    Adición de router al index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import typerRouter from './routes/typers.js';
+import mensajeRouter from './routes/mensajes.js';
 
 const app = express();
 const PORT = 4000;
@@ -8,6 +9,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use("/typers", typerRouter);
+app.use("/mensajes", mensajeRouter);
 
 app.all("*", (req, res) => res.status(404).send(
     {success: false, 
