d851f3d028304dce596fd088eefcab2fd9e8d31b
Ángel Juárez García <61328294+lzeroxl@users.noreply.github.com>
2021-06-21 23:07:25 -0500

    Update MensajeDominio.cs

--- Models/MensajeDominio.cs
+++ Models/MensajeDominio.cs
@@ -7,19 +7,19 @@ namespace TypeMeWeb.Models
 {
     public class MensajeDominio
     {
-        [JsonPropertyName("IdMensaje")]
+        [JsonPropertyName("idMensaje")]
         public int IdMensaje { get; set; }
-        [JsonPropertyName("Contenido")]
+        [JsonPropertyName("contenido")]
         public string Contenido { get; set; }
-        [JsonPropertyName("Fecha")]
+        [JsonPropertyName("fecha")]
         public DateTime Fecha { get; set; }
         [JsonIgnore]
         public String Hora { get {return Fecha.ToString("HH:mm");} set {} }
-        [JsonPropertyName("IdGrupo")]
+        [JsonPropertyName("idGrupo")]
         public int IdGrupo { get; set; }
-        [JsonPropertyName("Typer")]
+        [JsonPropertyName("typer")]
         public Typer Typer { get; set; }
-        [JsonPropertyName("IdMultimedia")]
+        [JsonPropertyName("idMultimedia")]
         public string IdMultimedia { get; set; }
     }
 }
